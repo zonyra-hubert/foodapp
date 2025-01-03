fbce4746857927154a5a6f616fad69c96b9ee76d
Zonyra Hubert <123190677+zonyra-hubert@users.noreply.github.com>
2025-01-03 00:55:20 +0000

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import styles from "./search.module.css";
 const URL = "https://api.spoonacular.com/recipes/complexSearch"
 
-const API_KEY = "3c2811fa94ff4f65982c0ce4b700b70d"
+const API_KEY = "3c2811fa94ff4f65982c0ce4b700xxxx"
 export default function Search({foodData, setFoodData}) {
     const [query, setQuery] = useState("pizza"); 
 
